7df260b727548904766add055a3fb126f8d8c7a2
s1f10210254 <s1f10210254@users.noreply.github.com>
2023-07-06 04:52:33 +0000

    deploy: de8aa6cb0394ed554de67d3304b03f4828055d2f

--- _next/static/1UrHJa1vvrUWYXOsaFd6R/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/9b18141bde4a69f3.css","static/chunks/pages/index-87ec583d8dafb374.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
